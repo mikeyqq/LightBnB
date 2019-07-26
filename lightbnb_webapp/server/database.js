1ce2e8b49324ff9b956454bba44fbe0b841564b8
Anchen Huang <acmhuang@gmail.com>
2019-07-26 12:38:20 +0000

    added queries sql to functions in search page

--- lightbnb_webapp/server/database.js
+++ lightbnb_webapp/server/database.js
@@ -90,15 +90,69 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool
-    .query(
-      `
-  SELECT * FROM properties
-  LIMIT $1
-  `,
-      [limit]
-    )
-    .then(res => res.rows);
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  let qLength = false;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+    if (!qLength) {
+      qLength = true;
+    }
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(Number(options.minimum_price_per_night) * 100);
+    if (qLength) {
+      queryString += `AND cost_per_night >= $${queryParams.length}`;
+    } else {
+      qLength = true;
+      queryString += `WHERE cost_per_night >= $${query.Params.length}`;
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(Number(options.maximum_price_per_night) * 100);
+    if (qLength) {
+      queryString += `AND cost_per_night <= $${queryParams.length}`;
+    } else {
+      qLength = true;
+      queryString += `WHERE cost_per_night <= $${queryParams.length}`;
+    }
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(Number(options.minimum_rating));
+    if (qLength) {
+      queryString += `AND rating >= $${queryParams.length}`;
+    } else {
+      qLength = true;
+      queryString += `WHERE rating >= $${queryParams.length}`;
+    }
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
